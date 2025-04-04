f4e2edf649a6042af0a3bcaa91c184b347345470
novikoff <a.novikoff@bitmaker.fi>
2025-04-04 18:33:30 +0200

    PacketData alignment

--- src/packet/data.rs
+++ src/packet/data.rs
@@ -4,30 +4,36 @@ use crate::dpdk::ffi::RteMbuf;
 /// Структура для хранения данных пакета
 #[repr(C, align(64))]
 pub struct PacketData {
+    pub data_ptr: *const u8,
+    pub data_len: usize,
+
     pub source_port: u16,
     pub dest_port: u16,
     pub queue_id: u16,
+    pub _padding: u16,
+
     pub source_ip_ptr: *const u8,
     pub source_ip_len: usize,
     pub dest_ip_ptr: *const u8,
     pub dest_ip_len: usize,
-    pub data_ptr: *const u8,
-    pub data_len: usize,
     pub mbuf_ptr: *mut RteMbuf,
 }
 
 impl PacketData {
     pub fn new() -> Self {
         Self {
+            data_ptr: std::ptr::null(),
+            data_len: 0,
+
             source_port: 0,
             dest_port: 0,
             queue_id: 0,
+            _padding: 0,
+
             source_ip_ptr: std::ptr::null(),
             source_ip_len: 0,
             dest_ip_ptr: std::ptr::null(),
             dest_ip_len: 0,
-            data_ptr: std::ptr::null(),
-            data_len: 0,
             mbuf_ptr: std::ptr::null_mut(),
         }
     }
